32bd7373ec0910662a0dac59ff95a3e7fc1aa54c
atena caselli <atena@UniCredits-MacBook-Pro.local>
2023-06-08 18:03:20 +0200

    feat: add 404 page

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -2,11 +2,18 @@
 </script>
 
 <template>
-  <div>
-    <p>404</p>
-  </div>
+  <v-container fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" class="text-center">
+        <p class="text-h1">404</p>
+        <p>Page not found</p>
+        <p>URL not found</p>
+      </v-col>
+      <v-col cols="12" class="text-center">
+        <img src="../assets/incredible_hulk_last_call_1.jpeg" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
